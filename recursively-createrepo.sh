d274df832626925e93f6f2d903d31003db53f192
jhegg <josh.hegg@gmail.com>
2018-03-17 14:22:57 -0700

    Don't run createrepo on repodata directories
    Also verify that RPM files exist in the mounted directory.

--- recursively-createrepo.sh
+++ recursively-createrepo.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Recursively runs createrepo, starting from the base of the mounted volume.
-for directory in `find /usr/share/nginx/html -type d`; do
-  echo Running createrepo ${directory}
-  createrepo ${directory}
-done
+FOUND_RPMS=$(find /usr/share/nginx/html -type f -name '*.rpm' -print -quit)
 
+if test -n "$FOUND_RPMS"
+then
+  # For each non-repodata directory, run createrepo
+  for directory in `find /usr/share/nginx/html -type d -not -path "*/repodata"`; do
+    echo "Running createrepo on: ${directory}"
+    createrepo ${directory}
+  done
+else
+  echo "No RPM files were found; was the correct directory mounted into Docker?"
+  exit 1
+fi
